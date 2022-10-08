550506871c527316a52bc964fe15065e48709e43
Taekang Kim <zizoen123@gmail.com>
2022-10-08 16:26:02 +0900

    #2 dev:config-server에 application.yml 이동

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -4,7 +4,7 @@ import com.example.userservice.dto.UserRequest;
 import com.example.userservice.dto.UserResponse;
 import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.cfg.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +14,18 @@ import java.util.List;
 @RequestMapping("/user-service")
 @RestController
 public class UserController {
-
+    private final Environment env;
     private final UserService userService;
 
     @GetMapping("/health_check")
     public String status() {
-        return "It's Working in User Service on PORT";
+        return String.format("It's Working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", gateway ip=" + env.getProperty("gateway.ip")
+                + ", message=" + env.getProperty("greeting.message")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time"));
     }
 
     /**
